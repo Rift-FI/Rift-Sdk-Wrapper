40fe04dc95365931e1ac6a901f6fbc4766415e29
amschel99 <6290.2020@students.ku.ac.ke>
2026-04-08 15:26:35 +0300

    fix

--- src/controllers/deposits.ts
+++ src/controllers/deposits.ts
@@ -11,7 +11,7 @@ export const getAllDeposits = async (req: Request, res: Response) => {
 
 export const getDepositStats = async (req: Request, res: Response) => {
   try {
-    const response = await req.rift!.deposits.getDepositStats();
+    const response = await (req.rift!.deposits as any).getDepositStats();
     res.status(200).json(response);
   } catch (error: any) {
     res.status(400).json({ error: error.message });
